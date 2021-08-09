d3adfba182bf3849075005748b5d88d53aeaf122
Tridib Panda <pandatridib@gmail.com>
2021-08-09 17:10:38 +0530

    CNUE-12

--- App.js
+++ App.js
@@ -1,49 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import AppLoading from 'expo-app-loading';
-import * as Font from 'expo-font';
 import ReduxThunk from 'redux-thunk';
 
-import productsReducer from './store/reducers/products';
-import cartReducer from './store/reducers/cart';
-import ordersReducer from './store/reducers/orders';
-import authReducer from './store/reducers/auth';
-import NavigationContainer from './navigation/NavigationContainer';
+import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+import { init } from './helpers/db';
+
+init()
+  .then(() => {
+    console.log('Initialized database');
+  })
+  .catch(err => {
+    console.log('Initializing db failed.');
+    console.log(err);
+  });
 
 const rootReducer = combineReducers({
-  products: productsReducer,
-  cart: cartReducer,
-  orders: ordersReducer,
-  auth: authReducer
+  places: placesReducer
 });
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
-
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => {
-          setFontLoaded(true);
-        }}
-        onError={err => console.log(err)}
-      />
-    );
-  }
+  console.disableYellowBox = true;
   return (
     <Provider store={store}>
-      <NavigationContainer />
+      <PlacesNavigator />
     </Provider>
   );
 }
---
+++ env.js
@@ -0,0 +1,8 @@
+const ENV = {
+    // googleApiKey: 'AIzaSyD-ZPcMh4YKvZJSo-H2C7ESf2PEDJWlcEc' (The provided API key is invalid.)
+    googleApiKey: 'AIzaSyBf9-hannYNyHGloCBn-cr55EDDBqrD6Bo' /*The Google Maps Platform server rejected your request. 
+    You must enable Billing on the Google Cloud Project at https://console.cloud.google.com/project/_/billing/enable 
+    Learn more at https://developers.google.com/maps/gmp-get-started */
+};
+
+export default ENV;
